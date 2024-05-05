43188d3693cde95659c30e7f73b2512183ec949c
Rakkoon <nhattnm.ilt@gmail.com>
2024-05-05 11:02:12 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,27 @@
+import cv2
+from flask import Flask, render_template, request, send_file, jsonify
+from PIL import Image
+from detect import Detect
+import base64
+from flask_cors import CORS
+from io import BytesIO
+import os
+import requests
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/detect', methods=['POST'])
+def predict():
+    file = request.files['image']
+    plant_detected = dt.detect(file)
+    return jsonify({"plant_detected": plant_detected})
+
+if __name__ == '__main__':
+    dt = Detect()
+    app.run(port=5000, debug=True)
+
+
+
+
+

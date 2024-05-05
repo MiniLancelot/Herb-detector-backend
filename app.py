19ca5130b52af75a98f6d7f3a23311f2d980b6b5
Duc Nguyen <vuducnguyen007@gmail.com>
2024-05-05 21:09:17 +0700

    using with web socket

--- app.py
+++ app.py
@@ -8,21 +8,47 @@ from io import BytesIO
 import os
 import requests
 
-app = Flask(__name__)
-CORS(app)
+# app = Flask(__name__)
+# CORS(app)
+#
+#
+# @app.route('/detect', methods=['POST'])
+# def predict():
+#     file = request.files['image']
+#     plant_detected = dt.detect(file)
+#     return jsonify({"plant_detected": plant_detected})
+#
+# if __name__ == '__main__':
+#     dt = Detect()
+#     # app.run(port=5000, debug=True)
+#     app.run(host='0.0.0.0', debug=True)
 
+from socketio import Client
+from detect import Detect
 
-@app.route('/detect', methods=['POST'])
-def predict():
-    file = request.files['image']
-    plant_detected = dt.detect(file)
-    return jsonify({"plant_detected": plant_detected})
+class DetectClient(Client):
+    def __init__(self):
+        super().__init__()
 
-if __name__ == '__main__':
+    def on_image(self, image_data):
+        # Detect the plant in the image
+        plant_detected = dt.detect(image_data)
+
+        # Emit the detected plant data back to the server
+        self.emit('image', plant_detected)
+        return plant_detected
+
+if __name__ == "__main__":
     dt = Detect()
-    # app.run(port=5000, debug=True)
-    app.run(host='0.0.0.0', debug=True)
 
+    # Kết nối tới server
+    client = DetectClient()
+    client.connect('http://localhost:3000')
+
+    client.on('image', client.on_image)
+
+    # Bắt đầu lắng nghe sự kiện từ server
+    client.wait()
 
 
 
--- detect.py
+++ detect.py
@@ -1,3 +1,63 @@
+# import pathlib
+# import cv2
+# from PIL import Image
+# import torch
+# import base64
+# import numpy as np
+# from io import BytesIO
+#
+# class Detect:
+#     def __init__(self):
+#         temp = pathlib.PosixPath
+#         pathlib.PosixPath = pathlib.WindowsPath
+#         torch.hub._validate_not_a_forked_repo = lambda a, b, c: True
+#         self.model = torch.hub.load('ultralytics/yolov5', 'custom', path='best.pt', force_reload=True)
+#         pathlib.PosixPath = temp
+#
+#     def detect(self, image):
+#         # Read image
+#         image = cv2.imdecode(np.frombuffer(image.read(), np.uint8), cv2.IMREAD_COLOR)
+#         converted = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+#         converted = Image.fromarray(converted)
+#
+#         results = self.model(converted, size=640)
+#         labels = results.names
+#         plant_labels_detected = {"plants": {}}
+#
+#         for *box, conf, cls in results.xyxy[0]:
+#             label = f"{labels[int(cls)]}: {conf:.2f}"
+#
+#             cv2.rectangle(image, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (255, 0, 0), 2)
+#             width = box[2] - box[0]
+#             font_scale = get_optimal_font_scale(label, width)
+#             # changing scale
+#             # cv2.putText(image, label, (int(box[0]), int(box[1] - 10)), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 0, 255), 2)
+#             cv2.putText(image, label, (int(box[0]), int(box[1] - 10)), cv2.FONT_HERSHEY_SIMPLEX, font_scale,
+#                         (0, 0, 255), 2)
+#             # Count the number of fruit detected
+#             plant_labels_detected["plants"][labels[int(cls)]] = plant_labels_detected["plants"].get(labels[int(cls)],
+#                                                                                                     0) + 1
+#
+#         # pic = cv2.imwrite(f'img/{image_path}_detected.jpg', image)
+#
+#         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+#         pil_img = Image.fromarray(image)
+#         byte_arr = BytesIO()
+#         pil_img.save(byte_arr, format='JPEG')
+#         encoded_image = base64.b64encode(byte_arr.getvalue()).decode('utf-8')
+#
+#         plant_labels_detected["image"] = encoded_image
+#         # return image
+#         return plant_labels_detected
+#
+# def get_optimal_font_scale(text, width):
+#     for scale in reversed(range(0, 60, 1)):
+#         textSize = cv2.getTextSize(text, fontFace=cv2.FONT_HERSHEY_DUPLEX, fontScale=scale/10, thickness=1)
+#         new_width = textSize[0][0]
+#         if (new_width <= width):
+#             return scale/10
+#     return 0.1
+
 import pathlib
 import cv2
 from PIL import Image
@@ -16,7 +76,9 @@ class Detect:
 
     def detect(self, image):
         # Read image
-        image = cv2.imdecode(np.frombuffer(image.read(), np.uint8), cv2.IMREAD_COLOR)
+        image_bytes = base64.b64decode(image)
+        nparr = np.frombuffer(image_bytes, np.uint8)
+        image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
         converted = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         converted = Image.fromarray(converted)
 
@@ -30,15 +92,10 @@ class Detect:
             cv2.rectangle(image, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (255, 0, 0), 2)
             width = box[2] - box[0]
             font_scale = get_optimal_font_scale(label, width)
-            # changing scale
-            # cv2.putText(image, label, (int(box[0]), int(box[1] - 10)), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 0, 255), 2)
             cv2.putText(image, label, (int(box[0]), int(box[1] - 10)), cv2.FONT_HERSHEY_SIMPLEX, font_scale,
                         (0, 0, 255), 2)
-            # Count the number of fruit detected
             plant_labels_detected["plants"][labels[int(cls)]] = plant_labels_detected["plants"].get(labels[int(cls)],
                                                                                                     0) + 1
-
-        # pic = cv2.imwrite(f'img/{image_path}_detected.jpg', image)
 
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         pil_img = Image.fromarray(image)
@@ -47,18 +104,15 @@ class Detect:
         encoded_image = base64.b64encode(byte_arr.getvalue()).decode('utf-8')
 
         plant_labels_detected["image"] = encoded_image
-        # return image
         return plant_labels_detected
+
 
 def get_optimal_font_scale(text, width):
     for scale in reversed(range(0, 60, 1)):
-        textSize = cv2.getTextSize(text, fontFace=cv2.FONT_HERSHEY_DUPLEX, fontScale=scale/10, thickness=1)
+        textSize = cv2.getTextSize(text, fontFace=cv2.FONT_HERSHEY_DUPLEX, fontScale=scale / 10, thickness=1)
         new_width = textSize[0][0]
-        if (new_width <= width):
-            return scale/10
+        if new_width <= width:
+            return scale / 10
     return 0.1
-
-
-
 
 
